d77ec03de8f4f6b1267f2c52eb6dda9aaa09a99d
dmitriyrusnak1 <dmitriy.rusnak.dev@gmail.com>
2023-12-04 17:38:40 +0200

    get static paths

--- src/app/library/[id]/page.js
+++ src/app/library/[id]/page.js
@@ -3,9 +3,9 @@ import Layout from "@/components/ui/layout/layout"
 import { readJsonFile } from "@/lib/localdata"
 
 const LibraryPostPage = ({ params }) => {
-    const data = readJsonFile("/public/json/data.json")
-
     const { id } = params
+
+    const data = readJsonFile("/public/json/data.json")
     const { posts } = data
 
     const post = posts.find(el => el.id == id)
@@ -16,6 +16,23 @@ const LibraryPostPage = ({ params }) => {
             <LibraryPost post={post} related={related}/>
         </Layout>
     )
+}
+
+export async function getStaticPaths() {
+    const res = readJsonFile("/public/json/data.json")
+    
+    const paths = []
+
+    res.posts.forEach(el => {
+        paths.push({
+            params: { id: el.id.toString() }
+        })
+    })
+
+    return {
+        paths,
+        fallback: "blocking"
+    }
 }
 
 export default LibraryPostPage
